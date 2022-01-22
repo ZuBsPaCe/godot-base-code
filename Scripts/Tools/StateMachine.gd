b2d8323308b373a245684d4bb8dddda5ff344592
ZuBsPaCe <kurt.rudin@gmx.net>
2022-01-22 23:52:53 +0100

    StateMachine now works properly with awaits and arbitrary wait-calls.

--- Scripts/Tools/StateMachine.gd
+++ Scripts/Tools/StateMachine.gd
@@ -2,38 +2,159 @@ class_name StateMachine
 extends Node
 
 
-var current_state: int = -1
-var next_state: int = -1
+var current: int = -1
+
+var _check_valid_states := false
+var _valid_states := PackedInt32Array()
+var _requested_states := PackedInt32Array()
+
+var _blocked := false
+var _enter_called := false
 
 var _wait_secs: float = 0.0
 
-signal leave_state(p_state: int)
-signal process_state(p_state: int)
-signal enter_state(p_state: int)
+var _leave_callback: Callable
+var _enter_callback: Callable
+var _process_callback: Callable
 
-func setup(p_initial_state: int = -1) -> void:
-	current_state = -1
-	next_state = p_initial_state
+
+func setup(
+		p_initial_state: int,
+		
+		p_enter_callback: Callable,
+		p_process_callback: Callable,
+		p_leave_callback: Callable) -> void:
+	
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	
+	current = -1
 	_wait_secs = 0.0
+	
+	_enter_callback = p_enter_callback
+	_process_callback = p_process_callback
+	_leave_callback = p_leave_callback
+	
+	set_state(p_initial_state)
+
 
 func set_state(p_next_state: int) -> void:
-	next_state = p_next_state
+	_requested_states.append(p_next_state)
+
+
+func set_state_immediate(p_next_state: int) -> void:
+	_requested_states.resize(0)
+	_requested_states.append(p_next_state)
 	_wait_secs = 0.0
+	_check_valid_states = false
+
+
+func add_valid_state(p_valid_state: int):
+	if !_check_valid_states:
+		_valid_states.resize(0)
+	_check_valid_states = true
+	_valid_states.append(p_valid_state)
+
 
 func wait(p_secs: float) -> void:
 	_wait_secs = p_secs
 
-func _process(delta):
-	if current_state != next_state:
-		if current_state >= 0:
-			emit_signal("leave_state", current_state)
-		current_state = next_state
-		emit_signal("enter_state", current_state)
+
+func _perform_leave():
+	assert(_enter_called)
 	
+	if _leave_callback.is_valid() && current >= 0:
+		await _leave_callback.call()
+	
+	_enter_called = false
+	_check_valid_states = false
+
+
+func _perform_enter():
+	assert(_requested_states.size() > 0)
+	assert(!_enter_called)
+	
+	var next := _requested_states[0]
+	_requested_states.remove_at(0)
+	
+	current = next
+	
+	if _enter_callback.is_valid():
+		await _enter_callback.call()
+	
+	_enter_called = true
+
+
+func _perform_process():
+	assert(_enter_called)
+	
+	if _process_callback.is_valid():
+		await _process_callback.call()
+
+
+func _process(delta):
+	if _blocked:
+		return
+	
+	if _perform_wait():
+		return
+	
+	if !_enter_called:
+		_blocked = true
+		await _perform_enter()
+		_blocked = false
+	
+	if _wait_requested():
+		return
+	
+	if _requested_states.size() == 0:
+		_blocked = true
+		await _perform_process()
+		_blocked = false
+	else:
+		while _requested_states.size() > 0:
+			var next := _requested_states[0]
+			
+			if current == next:
+				_requested_states.remove_at(0)
+				continue
+			
+			if _check_valid_states && !_valid_states.has(next):
+				printerr("StateMachine: Current state %d. Next State %d invalid." % [current, next])
+				_requested_states.remove_at(0)
+				continue
+			
+			_blocked = true
+			await _perform_leave()
+			_blocked = false
+			
+			if _wait_requested():
+				return
+			
+			_blocked = true
+			await _perform_enter()
+			_blocked = false
+			
+			if _wait_requested():
+				return
+			
+			_blocked = true
+			await _perform_process()
+			_blocked = false
+			
+			if _wait_requested():
+				return
+
+
+func _wait_requested() -> bool:
+	return _wait_secs > 0.0
+
+
+func _perform_wait() -> bool:
 	if _wait_secs > 0.0:
-		_wait_secs -= delta
+		_wait_secs -= get_process_delta_time()
 		if _wait_secs > 0.0:
-			return
+			return true
+			
 		_wait_secs = 0.0
 	
-	emit_signal("process_state", current_state)
+	return false
